14b0a3c00a26e066f17621c2337dfea532d1aec7
Drew Hinde <drewhinde@hotmail.com>
2020-09-08 12:46:50 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,13 @@
+window.addEventListener("load", function(){
+    var nodes = document.querySelectorAll('nav[data-action="expand"] *:first-child');
+    console.log(nodes);
+    for(var i=0; i< nodes.length; i++) {
+        nodes[i].addEventListener("click", function() {
+            if(this.parentNode.className == "open") {
+                this.parentNode.className = "";
+            } else {
+                this.parentNode.className = "open";
+            }
+        });
+    }
+});
